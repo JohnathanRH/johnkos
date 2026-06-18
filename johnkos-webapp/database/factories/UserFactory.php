7d4bed290b7803cf5dceff0c93b7bb2ffeb35feb
Dooughnut <kusotori25@gmail.com>
2026-06-18 23:29:08 +0800

    feat: implement user authentication schema and owner profile management view

--- johnkos-webapp/database/factories/UserFactory.php
+++ johnkos-webapp/database/factories/UserFactory.php
@@ -28,6 +28,7 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('123123123'),
+            'phone' => fake()->e164PhoneNumber(),
             'remember_token' => Str::random(10),
         ];
     }
